0227334b238f36cca3487bf95f4d9baf97a525bb
alvin0319 <alvin7810@naver.com>
2023-03-21 22:46:53 +0900

    bootstrap: fix format

--- src/main/java/org/jukeboxmc/Bootstrap.kt
+++ src/main/java/org/jukeboxmc/Bootstrap.kt
@@ -11,7 +11,7 @@ object Bootstrap {
     @JvmStatic
     fun main(args: Array<String>) {
         val logger = Logger()
-        logger.info("Starting JukeboxMC (Bedrock Edition " + Network.Companion.CODEC.getMinecraftVersion() + " with Protocol " + Network.Companion.CODEC.getProtocolVersion() + ")")
+        logger.info("Starting JukeboxMC (Bedrock Edition " + Network.CODEC.getMinecraftVersion() + " with Protocol " + Network.CODEC.getProtocolVersion() + ")")
         Server(logger)
     }
 }
